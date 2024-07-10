d223c4fd973ae12c8545ee671dc5d32462cbf0db
Rafael Clementi <rrclementirafael@gmail.com>
2024-07-10 08:43:09 -0400

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,41 @@
+import "@/styles/global.css"
+import { View, StatusBar } from "react-native"
+
+import { Slot } from "expo-router"
+
+
+import {
+    useFonts,
+    Inter_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium
+} from "@expo-google-fonts/inter"
+import { Loading } from "@/components/loading"
+
+
+
+export default function Layout() {
+
+    const [fontsLoades] = useFonts({
+        Inter_500Medium,
+        Inter_400Regular,
+        Inter_600SemiBold,
+    })
+
+    if (!fontsLoades) {
+        return <Loading />
+    }
+
+    return (
+        <View className="flex-1 bg-zinc-950">
+            <StatusBar
+                backgroundColor="transparent"
+                barStyle="light-content"
+                translucent
+            />
+            <Slot />
+        </View>
+
+
+    )
+}
